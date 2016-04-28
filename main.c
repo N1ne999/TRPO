785ac00b55ff6466f83c16dee6a1c68403297b74
N1ne999 <get9719@gmail.com>
2016-04-28 14:31:20 +0600

    full prog

--- main.c
+++ main.c
@@ -8,7 +8,15 @@ int main() {
 printf("Введите значения A, B, C \n");
 	scanf("%d %d %d", &a, &b, &c);
 
+d = b * b - 4 * a * c;
 
+	if (d >= 0) {
+		x1 = (-b + sqrt(d)) / 2 * a;
+		x2 = (-b - sqrt(d)) / 2 * a;
+		printf("Корни уравнения: %.2f и %.2f\n", x1, x2);
+	}
+	else
+		printf("Отрицательный дискриминант\n");
 
 	return 0;
 }
