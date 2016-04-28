28cd1cb31f4a27b24388d4df9848fec53e8e129c
N1ne999 <get9719@gmail.com>
2016-04-28 16:10:27 +0600

    1

--- main.c
+++ main.c
@@ -5,7 +5,7 @@ int main() {
 
 	int a,b,c;
 	float x1,x2,d;
-printf("Введите значения A, B, C \n");
+printf("Введите значения уравнения A, B, C \n");
 	scanf("%d %d %d", &a, &b, &c);
 
 d = b * b - 4 * a * c;
